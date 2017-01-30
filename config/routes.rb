e628f539212cab097abfb5af9bdc8bd569e5083d
g-mehra <gmehra@alumni.uwo.ca>
2017-01-29 23:37:11 -0500

    Refactoring routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,28 @@
-Rails.application.routes.draw do
+# Rails.application.routes.draw do
+#
+#   root 'pictures#index'
+#
+#   get 'pictures' => 'pictures#index'
+#
+#   get 'pictures/new' => 'pictures#new'
+#   post 'pictures' => 'pictures#create'
+#
+#   get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
+#   patch 'pictures/:id' => "pictures#update"
+#
+#
+#   get 'pictures/:id' => 'pictures#show', as: 'picture'
+# 
+#   delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
+#
+#   root 'pictures#index'
+#
+#
+# end
 
+
+Rails.application.routes.draw do
   root 'pictures#index'
 
-  get 'pictures' => 'pictures#index'
-
-  get 'pictures/new' => 'pictures#new'
-  post 'pictures' => 'pictures#create'
-
-
-  get 'pictures/:id' => 'pictures#show', as: 'picture'
+  resources :pictures
 end
